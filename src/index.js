2e655cd195f898a09916931ecea6eba6c5810ee5
Karla Pereira <karlapereira@gec.inatel.br>
2021-06-29 14:41:48 -0300

    crud and auth using javascript

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+// const app = express(); para iniciar a api
+app.use(bodyParser.json()); //para entender json
+app.use(bodyParser.urlencoded({extended: false}));
+//urlencoded para ele entender parametros via url
+//app.listen = porta que eu quero ouvir
+
+require('./app/controllers/index')(app);
+
+console.log('api ok')
+
+app.listen(3000)
